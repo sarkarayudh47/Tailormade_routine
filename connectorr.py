888b903e5b5f1c92fe8deff157555eb602837588
sarkarayudh47 <sarkarayudh47@gmail.com>
2024-02-04 14:30:35 +0530

    fixed all the bugs, database and learning rate connected.

--- connectorr.py
+++ connectorr.py
@@ -6,7 +6,8 @@ mydb = myconn.connect(
     database="bzww6voywepttascpl44")
 
 db_cursor = mydb.cursor()
-# db_cursor.execute("SELECT LR FROM subdata where Subjects='Array'")  # select Total Timing from subdata  (alter this while searchig for total time)
+val=0.2
+db_cursor.execute("update subdata set `LR`="+str(val)+" where Subjects = 'Queue'")  # select Total Timing from subdata  (alter this while searchig for total time)
 
 def fetchOutput(subjectName, columnName, multipleSubjects):
     elapsedTime=0
@@ -15,8 +16,15 @@ def fetchOutput(subjectName, columnName, multipleSubjects):
         fetchedOutput=db_cursor.fetchone()
         return fetchedOutput[0] 
     else:
-        columnList=['MondayTimings','TuesdayTimings','WednesdayTimings','ThursdayTimings','FridayTimings','SaturdayTimings','SundayTimings']
+        columnList=["MondayTimings","TuesdayTimings","WednesdayTimings","ThursdayTimings","FridayTimings","SaturdayTimings","SundayTimings"]
         for i in range(0,6):
              db_cursor.execute("SELECT " + columnList[i]+" FROM subdata where Subjects='"+subjectName+"'")
              fetchedOutput=db_cursor.fetchone()
              elapsedTime+=fetchedOutput[0]
+        return elapsedTime
+
+def storeOutput(subjectName, columnName, value):
+    columnList=["MondayTimings","TuesdayTimings","WednesdayTimings","ThursdayTimings","FridayTimings","SaturdayTimings","SundayTimings"]
+    if columnName not in columnList:
+        print("f")
+    
