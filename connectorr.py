231cc10ef0676528b385ff134dfde324e7d759c0
sarkarayudh47 <sarkarayudh47@gmail.com>
2024-02-13 05:56:15 +0530

    update function written, learning rate function fully operational

--- connectorr.py
+++ connectorr.py
@@ -6,25 +6,37 @@ mydb = myconn.connect(
     database="bzww6voywepttascpl44")
 
 db_cursor = mydb.cursor()
-val=0.2
-db_cursor.execute("update subdata set `LR`="+str(val)+" where Subjects = 'Queue'")  # select Total Timing from subdata  (alter this while searchig for total time)
+mydb.autocommit=True
+# val=0.78
+# strval=str(val)
+# val_one=f"update subdata set LR={strval} where id=3"
+# db_cursor.execute(val_one)  # select Total Timing from subdata  (alter this while searchig for total time)
 
-def fetchOutput(subjectName, columnName, multipleSubjects):
+def updateValue(value,column,id):
+    columnList=["MondayTimings","TuesdayTimings","WednesdayTimings","ThursdayTimings","FridayTimings","SaturdayTimings","SundayTimings"]
+    if column not in columnList:
+        db_cursor.execute(f"update subdata set {column} = {value} where id = {id}")
+    else:
+        currentVal=fetchOutput(id,column,False)
+        updatedVal=currentVal+value
+        db_cursor.execute(f"update subdata set {column} = {updatedVal} where id = {id}")
+    mydb.commit()
+
+def fetchOutput(idValue, columnName, multipleSubjects):
     elapsedTime=0
     if not multipleSubjects:
-        db_cursor.execute("SELECT " + columnName+" FROM subdata where Subjects='"+subjectName+"'")
+        db_cursor.execute(f"SELECT {columnName} FROM subdata where id={idValue}")
         fetchedOutput=db_cursor.fetchone()
         return fetchedOutput[0] 
     else:
         columnList=["MondayTimings","TuesdayTimings","WednesdayTimings","ThursdayTimings","FridayTimings","SaturdayTimings","SundayTimings"]
-        for i in range(0,6):
-             db_cursor.execute("SELECT " + columnList[i]+" FROM subdata where Subjects='"+subjectName+"'")
+        for columnNameLoopVariable in columnList:
+             db_cursor.execute(f"SELECT {columnNameLoopVariable} FROM subdata where id={idValue}")
              fetchedOutput=db_cursor.fetchone()
              elapsedTime+=fetchedOutput[0]
         return elapsedTime
-
-def storeOutput(subjectName, columnName, value):
-    columnList=["MondayTimings","TuesdayTimings","WednesdayTimings","ThursdayTimings","FridayTimings","SaturdayTimings","SundayTimings"]
-    if columnName not in columnList:
-        print("f")
-    
+# updateValue(1,"LR",3)
+# def storeOutput(subjectName, columnName, value):
+#     columnList=["MondayTimings","TuesdayTimings","WednesdayTimings","ThursdayTimings","FridayTimings","SaturdayTimings","SundayTimings"]
+#     if columnName not in columnList:
+#         print("f")
--- learningRate.py
+++ learningRate.py
@@ -1,30 +1,43 @@
 import pandas as pd
 from connectorr import fetchOutput
-def learningRate(subject_name,obtained_marks,cutoff_marks,total_marks,first_exam,exam_flag): 
-    current_lr= fetchOutput(subject_name, 'LR', False)
-    sub_avgTime=fetchOutput(subject_name, 'AvgTime', False)
-    sub_elapsedTime=fetchOutput(subject_name,'abc', True)
+from connectorr import updateValue
+def learningRate(subject_id,obtained_marks,cutoff_marks,total_marks,first_exam,exam_flag): 
+    current_lr= fetchOutput(subject_id, 'LR', False)
+    print(current_lr)
+    sub_avgTime=fetchOutput(subject_id, 'AvgTime', False)
+    print(sub_avgTime)
+    sub_elapsedTime=fetchOutput(subject_id,'abc', True)
+    # Total_learning_Time=0.0
     Total_learning_Time=0.0
     if not exam_flag:                                           #jodi student exam na diye thake, tokhon jodi ei function ta call hoye tahole 'if' block ta call hobe, karon exam neoa hoye ni mane holo oke porte bola hocche, tar manei okei time recommend kora hocche 
         Total_learning_Time=current_lr*sub_avgTime  
-        print("this is the total learning time "+ str(Total_learning_Time))#ekhane ami oke ekta total learning time recommend korchi based on average time and learning rate jeta kina initially 1
+        print("this is the total learning time "+ str(Total_learning_Time))#ekhane ami oke ekta total learning time recommend korchi based on average time and learning rate jeta kina initially 1        
+    
+    
     elif exam_flag:                                             #exam niye marks peye gele ei block ta execute hobe
         if obtained_marks>cutoff_marks:                         #jodi student kore
             if not first_exam:                                  #ei jayega ta dekhche j ei current exam ta student er ei SUBJECT a first exam noy kina
                 lr=sub_elapsedTime/sub_avgTime                  #eisob sub_elapsed, sub_avg etc pore fetch korte db theke sub name take use kore
                 current_lr=(current_lr+lr)/2
-                print("after atleast one attempt, learning rate is "+ str(current_lr))#etake store korte hobe
+                print("after atleast one attempt, learning rate is "+ str(current_lr))
             else:                                               #jodi first exam hoye r bhalo marks paye tahole porer bar learning rate kichu ta komate chai jate next time oke average time er 30 min kom porte chaye
-                lr=(sub_avgTime-0.5)/sub_avgTime                #karon ami protibar bhalo number pele 1/2 hour kore komate chai
+                new_lr=fetchOutput(subject_id, 'LR', False)
+                print(new_lr)
+                currentLearningTime=new_lr*sub_avgTime 
+                print(currentLearningTime) 
+                lr=(currentLearningTime-0.5)/sub_avgTime                  #karon ami protibar bhalo number pele 1/2 hour kore komate chai
                 current_lr=lr  
                 print("after First attempt success, learning rate is "+ str(current_lr))#update korchi learning rate ta
+            updateValue(current_lr,"LR",subject_id)   
+             
         else:
         #forced_exam?
             Total_learning_Time=sub_avgTime-((obtained_marks/total_marks)*sub_avgTime)#jodi marks baaje paye oke aro porte bolbo subject ta based on the amount of marks
+            
             print("this is the additional learning time "+str(Total_learning_Time))
-    return Total_learning_Time
+    updateValue(Total_learning_Time,"TotalTiming",subject_id)
 
-
+learningRate(3,50,45,100,True,True)
 
 import datetime
 def dayName():
@@ -44,7 +57,7 @@ def dayName():
    #sub_avgTime
    #total_time
    
-testOutput=learningRate("Queue",80,70,100,True,True)
+
 
    
    
